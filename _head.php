e1e69756c8ebfa5e607f5b45b51ce033788ada32
niky.uma@gmail.com <niky.uma@gmail.com>
2018-10-01 10:10:07 +0530

    casas

--- _head.php
+++ _head.php
@@ -10,6 +10,7 @@
       <title>Your Kids 'R' Our Kids</title>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="stylesheet" href="css/owl.carousel.min.css">
       <link rel="stylesheet" href="css/owl.theme.default.min.css">
       <link rel="stylesheet" media="all" href="css/pluggins.css" />
